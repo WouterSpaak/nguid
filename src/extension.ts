939b1bba857433217f914e071e40abcec75479e1
Wouter Spaak <w.spaak@gmail.com>
2019-02-28 14:07:58 +0100

    whitespace

--- src/extension.ts
+++ src/extension.ts
@@ -4,7 +4,6 @@ import { guid } from './guid';
 export function activate(context: vscode.ExtensionContext) {
   const commandRef = vscode.commands.registerCommand('extension.nguid', () => {
     const editor = vscode.window.activeTextEditor;
-
     if (editor) {
       editor.edit(builder => {
         builder.insert(editor.selection.active, guid());
